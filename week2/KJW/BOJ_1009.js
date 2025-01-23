0491a41ff7cedf6f30e4b9d9c0d8c47e77ad1599
zwjddn1105 <zwjddn1105@naver.com>
2025-01-23 22:57:50 +0900

    BOJ_1009.js

--- week2/KJW/BOJ_1009.js
+++ week2/KJW/BOJ_1009.js
@@ -1,22 +1,32 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [n, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
+const fs = require("fs")
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt"
+const [n, ...input] = fs.readFileSync(filePath).toString().trim().split("\n")
 
-for (let i = 1; i <= Number(n); i++) {
-  const a = Number(input[0]);
-  const b = Number(input[1]);
-  let arr = [];
-  while (true) {
-    let num = a * b;
-    let len = String(a * b).length;
-    let last_num = num % (10 * len);
-    if (last_num in arr) {
-      break;
+for (let i = 0; i <= Number(n) - 1; i++) {
+  let arr = []
+  const nums = input[i].split(" ")
+  const a = Number(nums[0])
+  const b = Number(nums[1])
+  if (a % 10 === 0) {
+    console.log(10)
+    continue
+  }
+  let num = a
+  for (let j = 1; j <= b; j++) {
+    let last_num = num % 10
+    if (arr.includes(last_num)) {
+      break
     } else {
-      arr.push(last_num);
+      arr.push(last_num)
+      num *= a
     }
   }
-}
 
-let result = arr[(b % arr.length) - 1];
-console.log(result);
+  if (b % arr.length === 0) {
+    let result = arr[arr.length - 1]
+    console.log(result)
+  } else {
+    let result = arr[(b % arr.length) - 1]
+    console.log(result)
+  }
+}
