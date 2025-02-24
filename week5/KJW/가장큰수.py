dbc94476847d6b4bfe1efec7dbfd63cd236babaf
kimjeongwoo <zwjddn1105@naver.com>
2025-02-24 16:36:10 +0900

    가장큰수.py 수정

--- week5/KJW/가장큰수.py
+++ week5/KJW/가장큰수.py
@@ -3,16 +3,7 @@ def solution(numbers):
     numbers.sort(key = lambda x : x*3, reverse=True)
 
     answer = "".join(numbers)
+    # 아오 이부분때문에
     if int(answer) == 0:
         answer = '0'
     return answer
-
-print(solution([3, 30, 34, 5, 9]))
-print(solution([6, 10, 2]))
-print(solution([0, 0, 0]))
-
-
-
-a = '0000'
-a = int(a)
-print(a)
