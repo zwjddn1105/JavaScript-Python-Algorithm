c31cb7794bcd1203e7287bac2e52651cc720c3a3
kimjeongwoo <zwjddn1105@naver.com>
2025-03-28 09:52:06 +0900

    좌표압축.js

--- week10/KJW/coordinatecompression.js
+++ week10/KJW/coordinatecompression.js
@@ -1,42 +1,21 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [n, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const lst = input[0].split(" ").map(Number);
-const data = input[0].split(" ").map(Number);
-data.sort((a, b) => {
+const n = Number(input[0]);
+const data = input[1].split(" ").map(Number);
+
+const sorted = [...new Set(data)].sort((a, b) => {
   return a - b;
 });
+const sortedObj = {};
 
-for (let i = 0; i < Number(n); i++) {
-  let ans = 0;
-  let left = 0;
-  let right = data.length - 1;
-  let idx = -1;
-  while (left <= right) {
-    let mid = parseInt((left + right) / 2);
-    if (data[mid] === lst[i]) {
-      idx = mid;
-      break;
-    } else if (data[mid] < lst[i]) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-  while (true) {
-    if (idx === 0) {
-      break;
-    }
-    if (data[idx] === data[idx - 1]) {
-      idx -= 1;
-    } else {
-      break;
-    }
-  }
-  // console.log(idx);
-  // const copied = data.slice(0, idx)
-  const mySet = new Set(data.slice(0, idx));
-  ans = mySet.size;
-  process.stdout.write(ans + " ");
+for (let i = 0; i < sorted.length; i++) {
+  sortedObj[sorted[i]] = i;
 }
+
+let answer = "";
+for (let i = 0; i < n; i++) {
+  answer += sortedObj[data[i]] + " ";
+}
+console.log(answer);
